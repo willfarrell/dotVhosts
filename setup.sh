677c27852597b5c6a9e52f2d977edafab0837e0c
will Farrell <will.farrell@gmail.com>
2013-05-31 04:51:16 -0400

    v 0.0.2

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ create_home() {
 download_files() {
 	log "Downloading repo files"
 	
-	curl -L https://github.com/willfarrell/.vhosts/tarball/0.0.1 | tar xz
+	curl -L https://github.com/willfarrell/.vhosts/tarball/0.0.2 | tar xz
 	# copy contents into .vhosts folder
 	for FILE in `ls -l`
 	do
